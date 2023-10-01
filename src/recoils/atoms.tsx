c35bd9bcc00a67cd6ca3e847ca3381bc859d9639
bkkom <seoy1108@gmail.com>
2023-10-01 20:14:26 +0900

    feat: timer state 생성 및 time formatting

---
+++ src/recoils/atoms.tsx
@@ -0,0 +1,22 @@
+import { atom, selector } from 'recoil';
+
+const MINUTE = 1; // standard : 25분
+const DEFAULT_MINUTE = MINUTE * 60;
+
+export const timerState = atom({
+  key: 'timerState',
+  default: DEFAULT_MINUTE,
+});
+
+export const timerSelector = selector({
+  key: 'timerSelector',
+  get: ({ get }) => {
+    const time = get(timerState);
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return {
+      minutes,
+      seconds,
+    };
+  },
+});
